233971d0a8e7546f7af4ce2fbac4b83a335e4f9c
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-19 18:50:06 +0530

    Add program to check character type

--- Day 008 ques 1.c
+++ Day 008 ques 1.c
@@ -1,7 +1,6 @@
 Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
 
 #include <stdio.h>
-
 int main() {
 
     char ch;
