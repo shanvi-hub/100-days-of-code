e3a0a04b0d903f9c41424b6a156e225d0d7a924f
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-19 18:18:43 +0530

    Create Day 003 ques 2.c

---
+++ Day 003 ques 2.c
@@ -0,0 +1,29 @@
+ Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include<stdio.h>
+int main(){
+    
+    float celsius;
+    float fahrenheit;
+
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9/5)+32;
+    printf("%f\n", fahrenheit);
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
