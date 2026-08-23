118ae8deca3b44ae8622c22f729e29a066613109
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-23 17:31:55 +0530

    Create Day 014 ques 2.c

---
+++ Day 014 ques 2.c
@@ -0,0 +1,34 @@
+Q28: Write a program to print the product of even numbers from 1 to n.
+
+  #include <stdio.h>
+
+   int main() {
+  
+    int n;
+    long long product = 1;
+  
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+  
+    printf("Output: %lld\n", product);
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+8 (2 * 4)
+
+Input 2:
+6
+Output 2:
+48 (2 * 4 * 6)
+
+*/
