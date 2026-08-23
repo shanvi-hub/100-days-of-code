ea41a99aea9e512cadc4dcdd52681faf81757109
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-23 17:23:34 +0530

    Create Day 014 ques 1.c

---
+++ Day 014 ques 1.c
@@ -0,0 +1,32 @@
+Q27: Write a program to print the sum of the first n odd numbers.
+
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+    int current_odd = 1;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        sum += current_odd;
+        current_odd += 2;
+    }
+    
+    printf("%d\n", sum);
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+
+*/
