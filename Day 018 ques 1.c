0a28c832b2bfb6127f169d825908d2a234cd9012
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-27 15:38:21 +0530

    Create Day 018 ques 1.c

---
+++ Day 018 ques 1.c
@@ -0,0 +1,30 @@
+Q35: Write a program to print all factors of a given number.
+
+  #include <stdio.h>
+   int main() {
+  
+    int num, i;
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+1 2 3 6
+
+Input 2:
+10
+Output 2:
+1 2 5 10
+
+*/
