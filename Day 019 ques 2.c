7f9f36086a374b7747f393b85cb7c8ab4947aa96
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-29 01:51:28 +0530

    Create Day 019 ques 2.c

---
+++ Day 019 ques 2.c
@@ -0,0 +1,33 @@
+Q38: Write a program to find the sum of digits of a number.
+
+#include <stdio.h>
+int main() {
+  
+    int num, sum = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        sum = sum + remainder; 
+        num = num / 10; 
+    }
+  
+    printf("Sum of digits: %d\n", sum);
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+*/
