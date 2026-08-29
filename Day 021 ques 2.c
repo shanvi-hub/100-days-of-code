b25536cd3beb0d8ae7cc58b36aa513443678a4e3
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-30 00:58:25 +0530

    Create Day 021 ques 2.c

---
+++ Day 021 ques 2.c
@@ -0,0 +1,38 @@
+Q42: Write a program to check if a number is a perfect number.
+
+#include <stdio.h>
+int main() {
+  
+    int num, i, sum = 0;
+
+    printf("Input: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num && num > 0) {
+        printf("Output: Perfect number\n");
+    } else {
+        printf("Output: Not perfect number\n");
+    }
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+Perfect number
+
+Input 2:
+10
+Output 2:
+Not perfect number
+
+*/
