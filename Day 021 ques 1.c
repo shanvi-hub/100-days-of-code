472383eff7e33d1df3c5ca042030ed892b904450
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-08-30 00:49:31 +0530

    Create Day 021 ques 1.c

---
+++ Day 021 ques 1.c
@@ -0,0 +1,44 @@
+Q41: Write a program to swap the first and last digit of a number.
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+  
+    int num, first, last, digits = 0, temp, divisor = 1;
+  
+    scanf("%d", &num);
+    
+    temp = num;
+    last = num % 10;
+
+    while (temp >= 10) {
+        temp = temp / 10;
+        divisor = divisor * 10;
+    }
+    first = temp;
+
+    if (num < 10) {
+        printf("%d", num);
+    } else {
+        int middle = (num % divisor) / 10;
+        int result = last * divisor + middle * 10 + first;
+      
+        printf("%d", result);
+    }
+    
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4231
+
+Input 2:
+1001
+Output 2:
+1001
+
+*/
