82e79857979c1f3486ba6b240d8f8afa724d13eb
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-09-01 17:18:35 +0530

    Create Day 023 ques 2.c

---
+++ Day 023 ques 2.c
@@ -0,0 +1,32 @@
+Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+  #include <stdio.h>
+   int main() {
+  
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
