39685d6ff8ce5d5002d5cd11ef9501e602106112
Shanvi Pachauri <shanvipachauri11@gmail.com>
2026-09-05 12:25:56 +0530

    Create Day 026 ques 2.c

---
+++ Day 026 ques 2.c
@@ -0,0 +1,55 @@
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+        
+#include <stdio.h>
+int main(){
+
+    int i, j;
+
+    for(i = 1; i <= 5; i++)
+    {
+        if(i <= 3)
+        {
+            for(j = 1; j <= 2*i-1; j++)
+                printf("*\n");
+        }
+        else
+        {
+            for(j = 1; j <= 2*(5-i)+1; j++)
+                printf("*\n");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
